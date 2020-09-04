cd022b484b805f02b0d4a30336ea11e205c7c2a7
thftgr <44352170+thftgr@users.noreply.github.com>
2020-09-04 23:16:22 +0900

    1

--- src/main/java/com/thftgr/test.java
+++ src/main/java/com/thftgr/test.java
@@ -37,8 +37,8 @@ public class test {
                 System.out.println(System.getProperty("https.proxyHost"));
                 try {
                     Thread.sleep(200);
-                    if (new test().generator("7e2757b7-badc-4303-8cd0-2eefa9d78e3b")) {
-//                    if (new test().generator("cf62e033-9e89-49c6-9830-5e61adc29ac5")) {
+//                    if (new test().generator("7e2757b7-badc-4303-8cd0-2eefa9d78e3b")) {
+                    if (new test().generator("cf62e033-9e89-49c6-9830-5e61adc29ac5")) {
                         dataCount += 1;
                         System.out.println("generated data : " + dataCount + "GB");
                         System.out.println((System.currentTimeMillis() - timetmp) + "ms\n");
@@ -47,13 +47,55 @@ public class test {
                         System.out.println("generate fail. | data : " + dataCount + "GB");
 
                     }
+//                    Thread.sleep(20000);
                 } catch (Exception e) {
                     System.out.println("generate fail. | data : " + dataCount + "GB");
                     proxyList.remove(i);
                 }
 
+
+
             }
         }
+    }
+
+
+
+    boolean generator(String referrer_id) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        String installId = new test().rand(11, false);
+
+        JsonObject data = new JsonObject();
+        data.addProperty("key", (new test().rand(43, false) + "="));
+        data.addProperty("install_id", installId);
+        data.addProperty("fcm_token", installId + ":APA91b" + new test().rand(134, false));
+        data.addProperty("referrer", referrer_id);
+        data.addProperty("warp_enabled", false);
+        data.addProperty("tos", new test().iosdate());
+        data.addProperty("type", "Android");
+        data.addProperty("locale", "ko-KR");
+
+        Request request = new Request.Builder()
+                .url("https://api.cloudflareclient.com/v0a" + new test().rand(3, true) + "/reg")
+                .addHeader("'Content-Type'", " 'application/json; charset=UTF-8',")
+                .addHeader("'Host'", " 'api.cloudflareclient.com',")
+                .addHeader("'Connection'", " 'Keep-Alive',")
+//                .addHeader("'Proxy-Connection'", " 'Keep-Alive',")
+                .addHeader("'Accept-Encoding'", " 'gzip',")
+                .addHeader("'User-Agent'", " 'okhttp/3.12.1'")
+                .post(RequestBody.create( MediaType.parse("application/json"), data.toString()))
+                .build();
+        Response response = client.newCall(request).execute();
+        JsonObject resp = (JsonObject) JsonParser.parseString(response.body().string());
+//        System.out.println(resp.toString());
+        client.connectionPool().evictAll();
+        System.out.println("status code : " + response.code());
+        return !resp.get("referrer").isJsonNull();
+    }
+
+    void setProxy(JsonObject ip_port) {
+        System.setProperty("https.proxyHost", ip_port.get("ip").getAsString());
+        System.setProperty("https.proxyPort", ip_port.get("port").getAsString());
     }
 
     String rand(int length, boolean intOnly) {
@@ -84,57 +126,5 @@ public class test {
     String iosdate() {
         String iostime = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
         return iostime.substring(0, iostime.length() - 1) + "+09:00";
-    }
-
-    boolean generator(String referrer_id) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        String installId = new test().rand(11, false);
-
-        JsonObject data = new JsonObject();
-        data.addProperty("key", (new test().rand(43, false) + "="));
-        data.addProperty("install_id", installId);
-        data.addProperty("fcm_token", installId + ":APA91b" + new test().rand(134, false));
-        data.addProperty("referrer", referrer_id);
-        data.addProperty("warp_enabled", false);
-        data.addProperty("tos", new test().iosdate());
-        data.addProperty("type", "Android");
-        data.addProperty("locale", "ko-KR");
-
-        Request request = new Request.Builder()
-                .url("https://api.cloudflareclient.com/v0a" + new test().rand(3, true) + "/reg")
-                .addHeader("'Content-Type'", " 'application/json; charset=UTF-8',")
-                .addHeader("'Host'", " 'api.cloudflareclient.com',")
-                .addHeader("'Connection'", " 'Keep-Alive',")
-                .addHeader("'Accept-Encoding'", " 'gzip',")
-                .addHeader("'User-Agent'", " 'okhttp/3.12.1'")
-                .post(RequestBody.create(MediaType.parse("application/json"), data.toString()))
-                .build();
-
-        Response response = client.newCall(request).execute();
-        JsonObject resp = (JsonObject) JsonParser.parseString(response.body().string());
-
-        client.connectionPool().evictAll();
-        System.out.println("status code : " + response.code());
-        return !resp.get("referrer").isJsonNull();
-    }
-
-    JsonArray updateProxyList(String url) {
-        JsonArray list = new JsonArray();
-        /*
-            call rest api
-            parse string data to json array
-                [
-                    {"ip":"String","port":"int"},
-                    {"ip":"String","port":"int"},
-                    {"ip":"String","port":"int"},
-                ]
-         */
-        return list;
-    }
-
-
-    void setProxy(JsonObject ip_port) {
-        System.setProperty("https.proxyHost", ip_port.get("ip").getAsString());
-        System.setProperty("https.proxyPort", ip_port.get("port").getAsString());
     }
 }
